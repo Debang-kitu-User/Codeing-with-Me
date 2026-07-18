93a6594126011b75730e6bf996d625a2a18ca0ba
Debang-kitu-User <debangspersonal@gmail.com>
2026-07-18 18:35:42 +0530

    Time: 1 ms (86.64%), Space: 44.6 MB (65.55%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,37 +1,28 @@
 class Solution {
     List<String> result = new ArrayList<>();
-
-    public static boolean isValid(String current) {
-        int count = 0;
-        for (char ch : current.toCharArray()) {
-            if (ch == '(') {
-                count++;
-            } else {
-                count--;
-            }
-            if (count < 0)
-                return false;
-        }
-        return count == 0;
-    }
-
-    public void parenthesis(StringBuilder current, int n) {
+    public void parenthesis(StringBuilder current, int n, int open, int close) {
         if (current.length() == 2 * n) {
-            if (isValid(current.toString()))
-                result.add(current.toString());
+            result.add(current.toString());
             return;
         }
-        current.append('(');
-        parenthesis(current, n);
-        current.deleteCharAt(current.length() - 1);
+        if(open < n) {
+            current.append('(');
+            parenthesis(current, n, open+1, close);
+            current.deleteCharAt(current.length() - 1);
+        }
 
-        current.append(')');
-        parenthesis(current, n);
-        current.deleteCharAt(current.length() - 1);
+        if(close < open) {
+            current.append(')');
+            parenthesis(current, n, open, close+1);
+            current.deleteCharAt(current.length() - 1);
+        }
     }
 
     public List<String> generateParenthesis(int n) {
-        parenthesis(new StringBuilder(), n);
+        int open =0;
+        int close =0;
+
+        parenthesis(new StringBuilder(), n, open, close);
         return result;
     }
 }
